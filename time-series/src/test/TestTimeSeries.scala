48989eca969ac092cb02b9cb3029f3be0c7b2ef9
ideapad <ideapad@lenovo.fritz.box>
2018-07-06 15:55:36 +0300

    Added comments and exception handling

--- time-series/src/test/TestTimeSeries.scala
+++ time-series/src/test/TestTimeSeries.scala
@@ -3,25 +3,55 @@ package test
 import scala.collection.immutable.SortedMap
 import scala.io.Source
 import scala.math.rint
+import java.io.FileNotFoundException
 
+/***
+* @author: PrachiC
+* Time-Series challenge - Reads file from local file path passed as argument
+* Reads a file with series of timeStamp and price ratio
+* Prints max value, min value, rolling_sum, occurrences for price ratio for the sliding window of 'T' sec
+* Date: 05 July 2018
+*/
 object TestTimeSeries { 
 
     def main(args: Array[String]): Unit = {
-      if(args == null || args.length == 0) {
+		if(args == null || args.length == 0) {
+			throw new Exception("Please enter the filePath for execution")
+		}
+		run(args(0)) 
+	}
+
+  /***
+   * Reads the file at specified local file path and prints the time-series
+   */
+	def run(path: String): Boolean = {
+	  var res : Boolean = false
+      if(path == null || path.length == 0) {
         throw new Exception("Please enter the filePath")
-      }
-      val path = args(0)
-      val lines = Source.fromFile(path).getLines().toList     
+      }      
+	  try {
+		  val lines = Source.fromFile(path).getLines().toList     
       
-      // Get a list of tuples in sorted order
-      val dataList = lines.map(parseLine).toList.toSeq.sortBy(_._1) 
-      printTimeSeries(dataList)            
+		  // Get a list of tuples in sorted order
+		  val dataList = lines.map(parseLine).toList.toSeq.sortBy(_._1) 
+		  res = printTimeSeries(dataList)
+	    } catch {
+		    case e: FileNotFoundException => {
+			    println(s"File not found at path : $path")
+			    //e.printStackTrace
+		    }
+		    case exp: Exception => {
+			    println(s"Exception occurred while executing time-series for path : $path")
+			    //exp.printStackTrace
+		    }
+	    }
+	    res
     } 
   
     /**
      * Prints time series for given input data
      */
-    def printTimeSeries(dataList: Seq[(Long,Double)]) = {
+    def printTimeSeries(dataList: Seq[(Long,Double)]) : Boolean = {
       // Initialing variables
       var max: Double = 0.0
       var min: Double = 0.0
@@ -53,6 +83,7 @@ object TestTimeSeries {
         println(s"$time \t$value \t$num \t$rollingSum \t$min \t$max")
       })      
       println("-----------------------------------------------------------------")
+	  true
   }
     
     /**
@@ -64,6 +95,9 @@ object TestTimeSeries {
     }    
 }
 
+/***
+ * Constants for time-series
+ */
 object Constants {
   val WindowTimeInSec = 60
   val TimePosition = 0
